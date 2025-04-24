3e6e87234733774038ff50ca8a491558f1cff6ba
Alla Yevlampiieva <modan1679@gmail.com>
2025-04-24 13:10:41 +0300

    Do task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,1 +1,20 @@
 "use strict";
+
+function isEnoughCapacity(products, containerSize) {
+  let productQuantity = 0;
+  const productQuantityList = Object.values(products);
+
+  for (const item of productQuantityList) {
+    productQuantity += item;
+  }
+
+  return productQuantity <= containerSize;
+}
+
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
